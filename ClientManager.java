d5edf6e8b10b8a0dfb21ba77a43050d11d78ca83
Rowan Simmons <42simsystreet@gmail.com>
2020-04-21 19:38:19 -0400

    Update ClientManager.java

--- ClientManager.java
+++ ClientManager.java
@@ -8,8 +8,43 @@
  * Updates console log. 
  */
 
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.*; 
+import java.net.*; 
+
 public class ClientManager {
 
+    private GameStartFrame start_frame;
+    private ClientFrame frame; // = new ClientFrame();
+    public ClientManager(){
 
+                start_frame = new GameStartFrame();
+                /*
+                start_frame.setLocationRelativeTo(null);
+        start_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        start_frame.setResizable(false);
+        start_frame.setVisible(true); */
+
+                frame = new ClientFrame();
+                
+
+    }
+
+    public void showStartFrame(){
+        start_frame.setLocationRelativeTo(null);
+        start_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        start_frame.setResizable(false);
+        start_frame.setVisible(true);
+    }
+
+    public void showGuiFrame(){
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
 } //end class
+	
 	
