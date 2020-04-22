a70e0a285c24d5795c60428ac76a450f5f23fe41
Rowan Simmons <42simsystreet@gmail.com>
2020-04-22 13:35:04 -0400

    updating start screen

--- ClueStartFrame.java
+++ ClueStartFrame.java
@@ -8,10 +8,18 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
 
 public class ClueStartFrame extends JFrame{
-	private JPanel contentPane, clue_logo_panel, button_panel; 
-	private JButton btnStartGame;
+	//private JPanel contentPane, clue_logo_panel, button_panel; 
+	//private JButton btnStartGame;
+
+	private JButton start_button;
+    private JPanel  button_panel, contentPane;
+    private JLabel image_panel, dice_panel;
 	/**
 	 * Create the frame.
 	 */
@@ -22,20 +30,34 @@ public class ClueStartFrame extends JFrame{
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		contentPane.setBackground(Color.BLACK);
+
+		ImageIcon image = new ImageIcon("resources\\clueLogo.jpg");
+		image_panel = new JLabel(image);
+		image_panel.setBounds(10, 10, 400, 260);
+		image_panel.setBackground(Color.white);
 		
-		clue_logo_panel = new JPanel();
-		clue_logo_panel.setBackground(Color.BLUE);
-		clue_logo_panel.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
-		clue_logo_panel.setBounds(6, 6, 438, 538);
-		contentPane.add(clue_logo_panel);
+		ImageIcon dice = new ImageIcon("resources\\temp_dice.jpg");
+		dice_panel = new JLabel(dice);
+		dice_panel.setBounds(10, 150, 430, 400);
+        dice_panel.setBackground(Color.white);
+
+        button_panel = new JPanel();
+        button_panel.setBounds(123,500,200,60);
+        button_panel.setBackground(Color.black);
+        start_button = new JButton("START GAME");
+        start_button.setFocusPainted(false);
+        start_button.setBackground(Color.black);
+        start_button.setForeground(Color.white);
+        Font b_font = new Font("Times New Roman", Font.PLAIN, 20);
+        start_button.setFont(b_font);
+		//button_panel.add(start_button);
 		
-		button_panel = new JPanel();
-		button_panel.setBounds(6, 556, 438, 116);
+		contentPane.add(dice_panel);
+		contentPane.add(image_panel);
 		contentPane.add(button_panel);
-		button_panel.setLayout(null);
 		
-		btnStartGame = new JButton("Start Game");
-		btnStartGame.addActionListener(new ActionListener() {
+		start_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				//keep frame in memory, do not dispose(), just don't show
@@ -44,8 +66,7 @@ public class ClueStartFrame extends JFrame{
 				new CharacterSelectFrame(new ServerConnection());
 			}
 		});
-		btnStartGame.setBounds(149, 53, 117, 29);
-		button_panel.add(btnStartGame);
+		button_panel.add(start_button);
 		
 		//pack();
 		setLocationRelativeTo(null);
