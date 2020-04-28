8b5961733346b68b44d34f23771fb551c8bc9949
Rowan Simmons <42simsystreet@gmail.com>
2020-04-28 15:47:30 -0400

    adding unavailable card

--- ClueGameConstants.java
+++ ClueGameConstants.java
@@ -1,20 +1,27 @@
 public final class ClueGameConstants {
+	
+		public static final int PORT = 6060;
+	
+		public static final String IP = "localhost";
+	
+		public static final int MAX_CHARACTERS = 6;
+		
+		public static final int MIN_CHARACTERS = 3;
+		
+		public static final int REQUEST_AVAILABLE_CHARACTERS = 1223419;
+		
+		public static final int CONFIRM_INDEX_AVAILABLE = 4532178;
+	
+		public static final int MARK_CHARACTER_AS_TAKEN = 839483;
+	
+		public static final int REQUEST_INDEED_CHARACTER_AVAILABLE = 2938314;
+	
+		public static final int IS_SELECTED_CHARACTER_AVAILABLE = 5671234;
 
-	public static final int PORT = 6060;
-
-	public static final String IP = "localhost";
-
-	public static final int MAX_CHARACTERS = 6;
+		
 	
-	public static final int MIN_CHARACTERS = 3;
-	
-	public static final int REQUEST_AVAILABLE_CHARACTERS = 1223419;
-	
-	public static final int CONFIRM_INDEX_AVAILABLE = 4532178;
-	
-	
-	//TODO add more constant variables as needed
-	
-	
-	
-}
+		//TODO add more constant variables as needed
+		
+		
+		
+	} //end class
