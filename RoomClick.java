74ccad18bfdcae902603986d237d7870cf4cf41a
Rowan Simmons <42simsystreet@gmail.com>
2020-05-03 15:10:20 -0400

    updates

--- RoomClick.java
+++ RoomClick.java
@@ -51,7 +51,7 @@ public class RoomClick {
 	}
 
 	public static String findInvalidTiles(HashMap<Integer, ArrayList<Integer>> map, int x, int y){
-		if(map.containsKey(x)){
+		if(map.containsKey(y)){
 			List<Integer> badColumsList = map.get(y);
 			for(int i = 0; i < badColumsList.size();i++){
 				if(badColumsList.get(i) == x)
@@ -86,7 +86,7 @@ public class RoomClick {
 		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
 		map.put(0, new ArrayList<Integer>(Arrays.asList(6,8,15,17)));
 		map.put(4, new ArrayList<Integer>(Arrays.asList(0)));
-		map.put(6, new ArrayList<Integer>(Arrays.asList(0,23)));
+		map.put(6, new ArrayList<Integer>(Arrays.asList(0,24)));
 		map.put(10, new ArrayList<Integer>(Arrays.asList(0)));
 		map.put(11, new ArrayList<Integer>(Arrays.asList(0)));
 		map.put(17, new ArrayList<Integer>(Arrays.asList(0)));
@@ -97,6 +97,7 @@ public class RoomClick {
 		map.put(18, new ArrayList<Integer>(Arrays.asList(23)));
 		map.put(24, new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8,10,11,
 						12,13,15,16,17,18,19,20,21,22,23)));
+
 		return map;
 	}
 }
