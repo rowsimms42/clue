9cf631c58073b9454de96749cb188a6f854589b9
Getaneh90 <43190153+Getaneh90@users.noreply.github.com>
2020-05-05 10:57:35 -0700

    Add files via upload

---
+++ Cards.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.List; 
+ 
+
+
+public class Cards {
+
+
+//private final List<CardShuffler> shuffled = new ArrayList<>(TOTAL); 
+
+
+
+public void createDeck(){
+ 
+ //ArrayList<CardShuffler> shuffled = new ArrayList<>(TOTAL); 
+
+}
+
+
+
+}
