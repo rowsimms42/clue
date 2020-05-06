5f51da0143ed6416da1ed7807e8998f06df4324b
salazjos <56235858+salazjos@users.noreply.github.com>
2020-05-05 19:56:27 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+
+import javax.smartcardio.Card;
+
 public class Player {
 
     private String name;
     private int[] locationArray = {0,0};
     private long Id;
+    private ArrayList<Card> playerCardDeck;
     
     public Player(long Id){
         this.Id = Id;
+        playerCardDeck = new ArrayList<Card>();
     }
 
     public long getPlayerId(){
@@ -24,6 +30,10 @@ public class Player {
         for (int i = 0; i <playerLocation.length; i++){
             this.locationArray[i] = playerLocation[i];
         }
+    }
+
+    public ArrayList<Card> getPlayerDeck(){
+        return playerCardDeck;
     }
 
 }
