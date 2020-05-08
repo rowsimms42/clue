c831be53cd91c94b7840704567eebcd8fcb11453
salazjos <56235858+salazjos@users.noreply.github.com>
2020-05-08 17:28:02 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -8,6 +8,7 @@ public class Player implements Serializable{
     private int[] locationArray = {0,0};
     private long Id;
     private ArrayList<Card> playerCardDeck;
+    private int currentXLocation, currentYLocation;
     
     public Player(long Id){
         this.Id = Id;
@@ -19,7 +20,9 @@ public class Player implements Serializable{
     }
 
     public void setCharacter(Characters character) {
-    	playerCharacter = character;
+        playerCharacter = character;
+        setCurrentXLocation(playerCharacter.getxStarting());
+        setCurrentYLocation(playerCharacter.getyStarting());
     }
     
     public Characters getCharacter() {
@@ -39,6 +42,25 @@ public class Player implements Serializable{
     public ArrayList<Card> getPlayerDeck(){
         return playerCardDeck;
     }
+
+    public void setCurrentXLocation(int value){
+        currentXLocation = value;
+    }
+
+    public void setCurrentYLocation(int value){
+        currentYLocation = value;
+    }
+
+    public int getCurrentXLocation(){
+        return currentXLocation;
+    }
+
+    public int getCurrentYLocation(){
+        return currentYLocation;
+    }
+
+
+
 
 }
 
