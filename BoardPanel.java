ea76c0bc8b4de2d055791e789ddf88cf7b578170
salazjos <56235858+salazjos@users.noreply.github.com>
2020-05-11 19:08:53 -0600

    Add files via upload

--- BoardPanel.java
+++ BoardPanel.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 
 public class BoardPanel extends JPanel {
 
-    final ImageIcon gameboard;
+	final ImageIcon gameboard;
     RoomClick roomClick;
     int rows = 24;
     int coloums = 25;
@@ -136,16 +136,13 @@ public class BoardPanel extends JPanel {
         btnRollDice.setBounds(579, 306, 99, 30);
         this.add(btnRollDice);
 
+        //request movement options at launch
         try {
-            requestBtns(currentXgrid, currentYgrid);
-        } catch (ClassNotFoundException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
+			requestBtns(currentXgrid, currentYgrid);
+		} catch (ClassNotFoundException | IOException e2) {
+			e2.printStackTrace();
+		}
+        
         enableOrdisableBtns(movementButtons, cArray);
 
         movementButtons[DOWN].addActionListener(new ActionListener() {
@@ -155,14 +152,11 @@ public class BoardPanel extends JPanel {
                 repaint();
 
                 try {
-                    requestBtns(currentXgrid, currentYgrid);
-                } catch (ClassNotFoundException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
+					requestBtns(currentXgrid, currentYgrid);
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+                
                 enableOrdisableBtns(movementButtons, cArray);
             }
         });
@@ -174,14 +168,11 @@ public class BoardPanel extends JPanel {
                 repaint();
 
                 try {
-                    requestBtns(currentXgrid, currentYgrid);
-                } catch (ClassNotFoundException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
+					requestBtns(currentXgrid, currentYgrid);
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+               
                 enableOrdisableBtns(movementButtons, cArray);
             }
         });
@@ -191,16 +182,13 @@ public class BoardPanel extends JPanel {
                 xC += 21;
                 repaint();
                 currentXgrid++;
-
+                
                 try {
-                    requestBtns(currentXgrid, currentYgrid);
-                } catch (ClassNotFoundException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
+					requestBtns(currentXgrid, currentYgrid);
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+                
                 enableOrdisableBtns(movementButtons, cArray);
             }
         });
@@ -210,16 +198,13 @@ public class BoardPanel extends JPanel {
                 xC -= 21;
                 repaint();
                 currentXgrid--;
-
+                
                 try {
-                    requestBtns(currentXgrid, currentYgrid);
-                } catch (ClassNotFoundException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
+					requestBtns(currentXgrid, currentYgrid);
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+                
                 enableOrdisableBtns(movementButtons, cArray);
             }
         });
@@ -241,70 +226,83 @@ public class BoardPanel extends JPanel {
         return new Rectangle(x,y,20,20);
     }
  
-private Boolean determineBounds(int x, int y)
-{
-    return (x>=23 && y>=13 && x<=536 && y<=511);
-}
+    private Boolean determineBounds(int x, int y){
+    	return (x>=23 && y>=13 && x<=536 && y<=511);
+    }
 
-
-private void requestBtns(int x, int y) throws IOException, ClassNotFoundException
-{
-    int[] coords = new int[2];
-
-    coords[0] = x;
-    coords[1] = y;
-    String xc = String.valueOf(coords[0]);
+    private void requestBtns(int x, int y) throws IOException, ClassNotFoundException{
+    	int[] coords = {0,0};
+    	coords[0] = x;
+    	coords[1] = y;
+    	String xc = String.valueOf(coords[0]);
                 String yc = String.valueOf(coords[1]);
                 String s = (", ");
                 String coordinates = xc.concat(s).concat(yc);
                 clientFrame.addToLogConsole(coordinates);
 
-    client.send(new Message(ClueGameConstants.REQUEST_MOVEMENT_BUTTON_VALUES, coords));
-    // Receive message with available connections from the server
-    messageReceived = client.getMessage();
-    // Get available connections from the message
-    btnValues =  (String) messageReceived.getData();
-    //put string into array
-    cArray = btnValues.toCharArray();
-    //System.out.println(Collections.singletonList(buttonValues));
-    clientFrame.addToLogConsole(btnValues);
-}
+        client.send(new Message(ClueGameConstants.REQUEST_MOVEMENT_BUTTON_VALUES, coords));
+        // Receive message with available connections from the server
+        messageReceived = client.getMessage();
+        // Get available connections from the message
+        btnValues =  (String) messageReceived.getData();
+        //put string into character array
+        cArray = btnValues.toCharArray();
+        //System.out.println(Collections.singletonList(buttonValues));
+        clientFrame.addToLogConsole(btnValues);
+    }
 
-private void enableOrdisableBtns(JButton movementButtons[], char cArray[])
-{
-    if (cArray[0] == '1')
-    {
-        movementButtons[LEFT].setEnabled(true);
+    private void enableOrdisableBtns(JButton movementButtons[], char cArray[]){
+    	/*
+		boolean []moveOptions = {false,false,false,false};
+    	for(int i = 0; i < 4; i++) {
+    		//moveOptions[i] = (cArray[i] == '1') ? true : false; ---OR if you want to use if/else
+    		if(cArray[i] == '1')
+    			moveOptions[i] = true;
+    		else
+    			moveOptions[i] = false;
+    	}
+    	movementButtons[LEFT].setEnabled(moveOptions[LEFT]);
+    	movementButtons[RIGHT].setEnabled(moveOptions[RIGHT]);
+    	movementButtons[UP].setEnabled(moveOptions[UP]);
+    	movementButtons[DOWN].setEnabled(moveOptions[DOWN]); */
+	
+    	if (cArray[LEFT] == '1')
+    	{
+    		movementButtons[LEFT].setEnabled(true);
+    	}
+    	else if (cArray[LEFT] == '0')
+    	{
+    		movementButtons[LEFT].setEnabled(false);
+    	} 
+    	if (cArray[RIGHT] == '1')
+    	{
+    		movementButtons[RIGHT].setEnabled(true);
+    	}
+    	else if (cArray[RIGHT] == '0')
+    	{
+    		movementButtons[RIGHT].setEnabled(false);
+    	}
+    	if (cArray[UP] == '1')
+    	{
+    		movementButtons[UP].setEnabled(true);
+    	}
+    	else if (cArray[UP] == '0')
+    	{
+    		movementButtons[UP].setEnabled(false);
+    	}
+    	if (cArray[DOWN] == '1')
+    	{
+    		movementButtons[DOWN].setEnabled(true);
+    	}
+    	else if (cArray[DOWN] == '0')
+    	{
+    		movementButtons[DOWN].setEnabled(false);
+    	}
     }
-    else if (cArray[0] == '0')
-    {
-        movementButtons[LEFT].setEnabled(false);
-    } 
-    if (cArray[1] == '1')
-    {
-        movementButtons[RIGHT].setEnabled(true);
+
+    private void requestBtnsAndEnableOrDisable(int x, int y, JButton movementButtons[], char cArray[]) {
+    	
     }
-    else if (cArray[1] == '0')
-    {
-        movementButtons[RIGHT].setEnabled(false);
-    }
-    if (cArray[2] == '1')
-    {
-        movementButtons[UP].setEnabled(true);
-    }
-    else if (cArray[2] == '0')
-    {
-        movementButtons[UP].setEnabled(false);
-    }
-    if (cArray[3] == '1')
-    {
-        movementButtons[DOWN].setEnabled(true);
-    }
-    else if (cArray[3] == '0')
-    {
-        movementButtons[DOWN].setEnabled(false);
-    }
-}
 
 } // end class
 
