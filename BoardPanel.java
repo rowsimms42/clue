20ee11cd854501d56a8aad668be9d08e730ed583
Rowan Simmons <42simsystreet@gmail.com>
2020-05-17 16:39:27 -0400

    adding back deleted code for buttons

--- BoardPanel.java
+++ BoardPanel.java
@@ -32,7 +32,7 @@ public class BoardPanel extends JPanel {
     BasicArrowButton[] movementButtons;
     JLabel boardLabel;
     JButton[] enterButton, roomButtons; //to hold buttons in room
-    int diceRollValue = 0, movementAmount = 0 ,previousMovement = 0;
+    int diceRollValue = 1, movementAmount = 0 ,previousMovement = 0;
     char[] cArray_movement_enter;
     int xC = 0; //x coordinate for drawing on board
     int yC = 0; //y coordinate for drawing on board
@@ -69,7 +69,8 @@ public class BoardPanel extends JPanel {
         requestBtns(currentXgrid, currentYgrid);
         //enabled or disable buttons at launch
         enableOrdisableBtns(movementButtons, enterButton, cArray_movement_enter);
-        
+        //movement buttons disabled
+        disableButtons(movementButtons);
         //request initial player map (for drawing)
         requestPlayerMap();
         repaint();
@@ -152,7 +153,9 @@ public class BoardPanel extends JPanel {
         
         btnRollDice.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		diceRollValue = requestDiceRoll();
+                diceRollValue = requestDiceRoll();
+                requestBtns(currentXgrid, currentYgrid);
+                enableOrdisableBtns(movementButtons, enterButton, cArray_movement_enter);
         	}
         });
         
@@ -181,16 +184,16 @@ public class BoardPanel extends JPanel {
     }
     
     private int requestDiceRoll(){
-    	int diceRoll = 0;
+    	//int diceRoll = 0;
         try {
 			client.send(new Message(ClueGameConstants.REQUEST_DICE_ROLL, null));
 			messageReceived = client.getMessage();
-			diceRoll =  (int) messageReceived.getData();
+			diceRollValue =  (int) messageReceived.getData();
 			clientFrame.addToLogConsole("Dice roll: " + diceRollValue);
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		} 
-        return diceRoll;
+        return diceRollValue;
     }
     
     private int getDoorId(int row, int col) {
@@ -330,23 +333,40 @@ public class BoardPanel extends JPanel {
     }
     
     private void enableOrdisableBtns(JButton movementButtons[], JButton enterButton[], char cArray_movement_enter[]){
-    	//WEST = 0, EAST = 1, NORTH = 2, SOUTH = 3;
-        boolean []moveOptions = {false,false,false,false};
-        boolean roomOptions = false;
-        int i=1;
-        //determine if there is an option to enter a room
-        roomOptions = (cArray_movement_enter[0] == '1') ? true : false;
-        //determine all movement options
-        for (int j=0; j<moveOptions.length; j++){
-        	moveOptions[j] = (cArray_movement_enter[i] == '1') ? true : false;
-            i++;
+        //WEST = 0, EAST = 1, NORTH = 2, SOUTH = 3;
+        if (diceRollValue == 0)
+        {
+            btnRollDice.setEnabled(false);
+            disableButtons(movementButtons);
         }
-        //enable or disable movement buttons
-    	movementButtons[WEST].setEnabled(moveOptions[WEST]);
-    	movementButtons[EAST].setEnabled(moveOptions[EAST]);
-    	movementButtons[NORTH].setEnabled(moveOptions[NORTH]);
-        movementButtons[SOUTH].setEnabled(moveOptions[SOUTH]);
-        enterButton[ENTER_ROOM].setEnabled(roomOptions);    
+        else
+        {
+            boolean []moveOptions = {false,false,false,false};
+            boolean roomOptions = false;
+            int i=1;
+            //determine if there is an option to enter a room
+            roomOptions = (cArray_movement_enter[0] == '1') ? true : false;
+            //determine all movement options
+            for (int j=0; j<moveOptions.length; j++){
+        	    moveOptions[j] = (cArray_movement_enter[i] == '1') ? true : false;
+                i++;
+            }
+            //enable or disable movement buttons
+    	    movementButtons[WEST].setEnabled(moveOptions[WEST]);
+    	    movementButtons[EAST].setEnabled(moveOptions[EAST]);
+    	    movementButtons[NORTH].setEnabled(moveOptions[NORTH]);
+            movementButtons[SOUTH].setEnabled(moveOptions[SOUTH]);
+            enterButton[ENTER_ROOM].setEnabled(roomOptions);    
+        }
+    }
+
+    public void disableButtons(JButton movementButtons[])
+    {
+        movementButtons[WEST].setEnabled(false);
+    	movementButtons[EAST].setEnabled(false);
+    	movementButtons[NORTH].setEnabled(false);
+        movementButtons[SOUTH].setEnabled(false);    
+        enterButton[ENTER_ROOM].setEnabled(false);
     }
     
     
